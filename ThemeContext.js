0f15b05dc71dfdcad5c97d85669bb469edfe7374
Srijith Mulupuri <165108998+Srijith1912@users.noreply.github.com>
2025-06-05 22:15:36 -0400

    Update ThemeContext.js

--- ThemeContext.js
+++ ThemeContext.js
@@ -1,21 +1,26 @@
 import React, { createContext, useContext, useState } from "react";
 
-const ThemeContent = createContext();
+const ThemeContext = createContext();
 
-export function ThemeProvider({children}){
-
+export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState('light');
 
   const toggleTheme = () => 
     setTheme(theme === 'light' ? 'dark' : 'light');
 
-  return (
-    <ThemeContent.Provider value={[theme, toggleTheme]}>
-      {children}
-    </ThemeContent.Provider>
-  );
+  const themeStyles = {
+    container: theme === 'light' ? 'bg-gray-100 text-black border border-gray-200 shadow-lg' : 'bg-gray-900 text-white border border-gray-700 shadow-lg',
+    input: theme === 'light' ? 'border-gray-300' : 'border-gray-600 bg-gray-800 text-white',
+    listItem: theme === 'light' ? 'bg-white shadow' : 'bg-gray-800 shadow',
+  };
 
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme, themeStyles }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
-  export function useTheme(){
-    return useContext(ThemeContent);
-  }
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
